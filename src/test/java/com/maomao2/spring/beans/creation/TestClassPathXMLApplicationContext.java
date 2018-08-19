f0f041bede8189a90ebdcd2675cb970e1223c5d1
tanxiazhe <tanxiazhe@163.com>
2018-08-19 21:35:10 +0800

    add test cases

--- src/test/java/com/maomao2/spring/beans/creation/TestClassPathXMLApplicationContext.java
+++ src/test/java/com/maomao2/spring/beans/creation/TestClassPathXMLApplicationContext.java
@@ -1,23 +1,56 @@
 package com.maomao2.spring.beans.creation;
 
+import static org.junit.Assert.assertEquals;
+
 import com.maomao2.spring.context.ClassPathXMLApplicationContext;
+import org.junit.Test;
 
 public class TestClassPathXMLApplicationContext {
 
-  public static void main(String[] args) {
+  @Test
+  public void test_constructorInject() {
     ClassPathXMLApplicationContext applicationContext = new ClassPathXMLApplicationContext("applicationContext.xml");
 
-//    Person person = applicationContext.getBean("person", Person.class);
-//    System.out.println(person);
-//
     Pad p1 = applicationContext.getBean("pad1", Pad.class);
-    System.out.println(p1.getPrice());
-//
-//    Pad p2 = applicationContext.getBean("pad2", Pad.class);
-//    System.out.println(p2.getPrice());
+    assertEquals(p1.getPrice(),1999.9,0.1);
+  }
 
-//    Person person2 = applicationContext.getBean("person2", Person.class);
-//    System.out.println(person2.getPad().getPrice());
+  @Test
+  public void test_constructorInject_lazyInit() {
+    ClassPathXMLApplicationContext applicationContext = new ClassPathXMLApplicationContext("applicationContext.xml");
+
+    Pad p2 = applicationContext.getBean("pad2", Pad.class);
+    assertEquals(p2.getPrice(),22,0.1);
+  }
+
+  @Test
+  public void test_propertyInject() {
+    ClassPathXMLApplicationContext applicationContext = new ClassPathXMLApplicationContext("applicationContext.xml");
+
+    Person person = applicationContext.getBean("person", Person.class);
+    System.out.println(person);
 
   }
+
+  @Test
+  public void test_constructorInject_ref() {
+    ClassPathXMLApplicationContext applicationContext = new ClassPathXMLApplicationContext("applicationContext.xml");
+
+    Person person2 = applicationContext.getBean("person2", Person.class);
+    assertEquals(person2.getPad().getPrice(),1999.9,0.1);
+  }
+
+
+  @Test
+  public void test_propertyInject_ref() {
+    ClassPathXMLApplicationContext applicationContext = new ClassPathXMLApplicationContext("applicationContext.xml");
+
+    Person person3 = applicationContext.getBean("person3", Person.class);
+    assertEquals(person3.getPad().getPrice(),22,0.1);
+  }
+
+
+
+
+
 }
