42a32519d144f3be8ee1bed50361b4b777148e35
tanxiazhe <tanxiazhe@163.com>
2018-08-20 16:38:04 +0800

    test

--- src/test/java/com/maomao2/spring/beans/creation/Pad.java
+++ src/test/java/com/maomao2/spring/beans/creation/Pad.java
@@ -2,24 +2,23 @@ package com.maomao2.spring.beans.creation;
 
 public class Pad {
 
-  private double price;
+    private double price;
 
-  public Pad() {
-    System.out.println("default constuctor");
-    this.price = 22;
-  }
+    public Pad() {
+        this.price = 22;
+    }
 
-  public Pad(double price) {
-    super();
-    this.price = price;
-  }
+    public Pad(double price) {
+        super();
+        this.price = price;
+    }
 
-  public double getPrice() {
-    return price;
-  }
+    public double getPrice() {
+        return price;
+    }
 
-  public void setPrice(double price) {
-    this.price = price;
-  }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
